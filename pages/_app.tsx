3c8ed56cc05d6836a766682a0abe923f1fe3e71b
lexermal <contact@weixler.me>
2022-06-02 23:26:29 +0200

    Added react-bootstrap; Added default navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
