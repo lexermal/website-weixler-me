473091abd47bacd42816cdc081e497b66695acbf
lexermal <contact@weixler.me>
2022-06-07 23:06:16 +0200

    Fixed mobile friendlyness

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Weixler.me</title>
         <meta name="description" content="Weixler.me" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <FixedNavbar />
