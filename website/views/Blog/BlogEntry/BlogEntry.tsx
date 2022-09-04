dd4ea70939d77168ba0aef73bc5a7c51433b835c
lexermal <contact@weixler.me>
2022-09-04 16:09:59 +0200

    Added source information

--- website/views/Blog/BlogEntry/BlogEntry.tsx
+++ website/views/Blog/BlogEntry/BlogEntry.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import Prism from "prismjs";
 require("prismjs/components/prism-yaml");
@@ -22,6 +22,18 @@ function BlogEntry(props: { content: string }) {
   return (
     <Container className="mt-5" id="blogEntry">
       <ReactMarkdown>{props.content}</ReactMarkdown>
+      <hr />
+      <Col>
+        <Row>
+          <span id="source">
+          For comments or improvements go
+            <a href="https://github.com/lexermal/main_website/tree/master/blog">
+              here
+            </a>
+          .
+          </span>
+        </Row>
+      </Col>
     </Container>
   );
 }
