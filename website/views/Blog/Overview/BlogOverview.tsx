7a39c1ec0da05a0161b32419f3da73f06c5b00f6
lexermal <contact@weixler.me>
2022-09-04 13:48:12 +0200

    Added basic navigation blog navigation

--- website/views/Blog/Overview/BlogOverview.tsx
+++ website/views/Blog/Overview/BlogOverview.tsx
@@ -1,5 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 function BlogOverview(props: { groups: string[] }) {
-  return <h1>My Blog{props.groups}</h1>;
+  return (
+    <div>
+      <h1>Weixler.me Blog</h1>
+
+      <Container>
+        <p>The following areas are available:</p>
+        {props.groups.map((e, k) => {
+          return (
+            <Row key={k}>
+              <Col>
+                <a href={"/blog/" + e}>
+                  <h2>{e}</h2>
+                </a>
+              </Col>
+            </Row>
+          );
+        })}
+      </Container>
+    </div>
+  );
 }
 
 export default BlogOverview;
